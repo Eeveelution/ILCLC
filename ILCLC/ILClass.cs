1375638fd8d47e547b9a83fff6364fa6312d0e44
Eevee <developerbrzoza@gmail.com>
2021-12-25 00:56:45 +0100

    ILClass: Beginning to work on ILClasses

--- ILCLC/ILClass.cs
+++ ILCLC/ILClass.cs
@@ -1,5 +1,15 @@
+using System.Collections.Generic;
+
 namespace ILCLC {
     public class ILClass {
-        
+        public bool   Public;
+        public bool   Private;
+        public bool   Static;
+        public string Name;
+
+        public List<ILField> Fields;
+        public List<ILMethod> Methods;
+
+
     }
 }
